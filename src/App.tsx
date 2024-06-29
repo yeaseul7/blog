87c996e27c5fa79a0fa92a223ae37ae7b776794b
seulseul_0123 <cutie32769@gmail.com>
2024-06-29 14:25:09 +0900

    fix : BrowserRouter 삭제
    삭제한 이유 : local에서 돌릴 때 항상 바꿔줘야함
    다른 방법이 있을거라고 생각해서 지움

--- src/App.tsx
+++ src/App.tsx
@@ -14,47 +14,45 @@ import PageModule from "./component/pageModule";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Router>
-        <div id="page" className="flex flex-col w-screen App">
-          <div
-            id="sideMenu"
-            className="flex flex-row items-center justify-around w-screen border-b shadow"
-          >
-            <h2 className="m-4 ml-12 text-2xl font-semibold">Neeko Blog</h2>
-            <div className="flex gap-4">
-              {menuList.map((menu, index) => (
-                <Link
-                  to={menu.path}
-                  key={index}
-                  className="inline-block linkStyle whitespace-nowrap "
-                >
-                  {menu.name}
-                </Link>
-              ))}
-              {/* <button className="linkStyle">테마</button> */}
-              {/* <button className="linkStyle">검색</button> */}
-            </div>
-          </div>
-          <div id="main">
-            <Routes>
-              <Route path="/" element={<PageModule type="introMd" />} />
-              {menuList.map((menu, index) => (
-                <Route
-                  key={index}
-                  path={menu.path}
-                  element={<menu.Component />}
-                />
-              ))}
-              <Route
-                path="/:type/:category/markdown/:name/:img"
-                element={<PageModule />}
-              />
-            </Routes>
+    <Router>
+      <div id="page" className="flex flex-col w-screen App">
+        <div
+          id="sideMenu"
+          className="flex flex-row items-center justify-around w-screen border-b shadow"
+        >
+          <h2 className="m-4 ml-12 text-2xl font-semibold">Neeko Blog</h2>
+          <div className="flex gap-4">
+            {menuList.map((menu, index) => (
+              <Link
+                to={menu.path}
+                key={index}
+                className="inline-block linkStyle whitespace-nowrap "
+              >
+                {menu.name}
+              </Link>
+            ))}
+            {/* <button className="linkStyle">테마</button> */}
+            {/* <button className="linkStyle">검색</button> */}
           </div>
         </div>
-      </Router>
-    </BrowserRouter>
+        <div id="main">
+          <Routes>
+            <Route path="/" element={<PageModule type="introMd" />} />
+            {menuList.map((menu, index) => (
+              <Route
+                key={index}
+                path={menu.path}
+                element={<menu.Component />}
+              />
+            ))}
+            <Route
+              path="/:type/:category/markdown/:name/:img"
+              element={<PageModule />}
+            />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
